4ff1880e441cf1a69119639bcd7e59cb6d83cf8b
Polina Bychkova <pbychkov@sfu.ca>
2021-08-28 20:44:20 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.4ec919255d6ce6538abb31eda418d4d7.js"
 );
 
 self.addEventListener('message', (event) => {
